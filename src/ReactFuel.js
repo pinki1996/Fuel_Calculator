938b1de7f03e45fbfc45dd18814a3e27d0179540
pinki1996 <120261749+pinki1996@users.noreply.github.com>
2024-01-27 18:57:28 +0530

    Update ReactFuel.js

--- src/ReactFuel.js
+++ src/ReactFuel.js
@@ -73,7 +73,7 @@ export const ReactFuel = () => {
             <div class="col-auto">
                 <label for="autoSizingInput">SPECIFIC GRAVITY: </label>
                 <input type='number' min="0" 
-        max="1000" step="0.001" class="form-control" id="autoSizingInput"  name='sp_gravity' onChange={handleValueChange} />
+        max="1000"  class="form-control" id="autoSizingInput"  name='sp_gravity' onChange={handleValueChange} />
             </div>
             
 
